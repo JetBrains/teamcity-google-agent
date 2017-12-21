256aacf62737a7c1d44686c4778474a8c42ce73d
Dmitry Tretyakov <dtretyakov@gmail.com>
2017-12-21 11:09:37 +0100

    Revert removed properties

--- google-cloud-server/src/main/kotlin/jetbrains/buildServer/clouds/google/GoogleCloudInstance.kt
+++ google-cloud-server/src/main/kotlin/jetbrains/buildServer/clouds/google/GoogleCloudInstance.kt
@@ -25,6 +25,8 @@ import jetbrains.buildServer.serverSide.AgentDescription
 class GoogleCloudInstance internal constructor(image: GoogleCloudImage, val id: String, val zone: String)
     : AbstractCloudInstance<GoogleCloudImage>(image, id, id) {
 
+    var properties: MutableMap<String, String> = HashMap()
+
     override fun containsAgent(agent: AgentDescription): Boolean {
         val agentInstanceName = agent.configurationParameters[GoogleAgentProperties.INSTANCE_NAME]
         return name.equals(agentInstanceName, ignoreCase = true)
