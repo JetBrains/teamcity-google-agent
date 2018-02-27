8e92c436daa573d286521f9d40b5a50ce5e1e992
Dmitry Tretyakov <dtretyakov@gmail.com>
2018-02-27 17:53:11 +0100

    Improve performance while checking GCE environment on agent start
    Issue: TW-51491

--- google-cloud-agent/src/main/kotlin/jetbrains/buildServer/clouds/google/GoogleMetadataReader.kt
+++ google-cloud-agent/src/main/kotlin/jetbrains/buildServer/clouds/google/GoogleMetadataReader.kt
@@ -26,10 +26,9 @@ import jetbrains.buildServer.clouds.CloudInstanceUserData
 import jetbrains.buildServer.util.EventDispatcher
 import org.apache.http.client.config.RequestConfig
 import org.apache.http.client.methods.HttpGet
-import org.apache.http.impl.client.CloseableHttpClient
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.util.EntityUtils
-import java.net.UnknownHostException
+import java.net.SocketTimeoutException
 
 class GoogleMetadataReader(events: EventDispatcher<AgentLifeCycleListener>,
                            private val configuration: BuildAgentConfigurationEx,
@@ -46,24 +45,38 @@ class GoogleMetadataReader(events: EventDispatcher<AgentLifeCycleListener>,
     }
 
     private fun fetchConfiguration() {
-        createHttpClient().use {
-            val response = try {
-                it.execute(HttpGet(METADATA_URL).apply {
-                    addHeader("Metadata-Flavor", "Google")
-                })
-            } catch (e: UnknownHostException) {
-                LOG.info("Google Compute integration is not available: could not access $METADATA_URL")
-                return@use
-            } catch (e: Exception) {
-                LOG.infoAndDebugDetails("Google Compute integration is not available: Failed to connect to $METADATA_URL: ${e.message}", e)
-                return@use
-            }
+        val requestConfig = RequestConfig.custom()
+                .setConnectTimeout(COMPUTE_PING_CONNECTION_TIMEOUT_MS)
+                .build()
 
-            val statusCode = response.statusLine.statusCode
-            if (statusCode == 200) {
-                updateConfiguration(EntityUtils.toString(response.entity))
-            } else {
-                LOG.info("Google Compute integration is not available: Failed to connect to $METADATA_URL: HTTP $statusCode")
+        HttpClients.custom()
+                .useSystemProperties()
+                .setDefaultRequestConfig(requestConfig)
+                .build().use {
+            for (i in 1..MAX_COMPUTE_PING_TRIES) {
+                val response = try {
+                    it.execute(HttpGet(instanceMetadataUrl).apply {
+                        addHeader(GCE_METADATA_HEADER, "Google")
+                    })
+                } catch (ignored: SocketTimeoutException) {
+                    // Ignore logging timeouts which is the expected failure mode in non GCE environments.
+                    continue
+                } catch (e: Exception) {
+                    LOG.info(ERROR_GCE_UNAVAILABLE + "Failed to connect to $metadataServerUrl: ${e.message}")
+                    return@use
+                }
+
+                val statusCode = response.statusLine.statusCode
+                if (statusCode != 200) {
+                    LOG.info(ERROR_GCE_UNAVAILABLE + "Failed to connect to $metadataServerUrl: HTTP $statusCode")
+                    return@use
+                }
+
+                if (response.getHeaders(GCE_METADATA_HEADER).any { it.value == "Google" }) {
+                    updateConfiguration(EntityUtils.toString(response.entity))
+                } else {
+                    LOG.info(ERROR_GCE_UNAVAILABLE + "Invalid $GCE_METADATA_HEADER header")
+                }
             }
         }
     }
@@ -105,11 +118,6 @@ class GoogleMetadataReader(events: EventDispatcher<AgentLifeCycleListener>,
         }
     }
 
-    private fun createHttpClient(): CloseableHttpClient {
-        val requestConfig = RequestConfig.custom().setSocketTimeout(TIMEOUT).setConnectTimeout(TIMEOUT).build()
-        return HttpClients.custom().useSystemProperties().setDefaultRequestConfig(requestConfig).build()
-    }
-
     data class Metadata(
             val attributes: MetadataAttributes?,
             val name: String,
@@ -130,9 +138,25 @@ class GoogleMetadataReader(events: EventDispatcher<AgentLifeCycleListener>,
 
     companion object {
         private val LOG = Logger.getInstance(GoogleMetadataReader::class.java.name)
-        private val METADATA_URL = "http://metadata.google.internal/computeMetadata/v1/instance/?recursive=true"
-        private val TIMEOUT = 10000
         private val GSON = Gson()
+
+        // Note: the explicit IP address is used to avoid name server resolution issues.
+        private const val DEFAULT_METADATA_SERVER_URL = "http://169.254.169.254"
+        private const val METADATA_API_URL = "/computeMetadata/v1/instance/?recursive=true"
+        private const val GCE_METADATA_HOST_ENV_VAR = "GCE_METADATA_HOST"
+        private const val GCE_METADATA_HEADER = "Metadata-Flavor"
+        private const val ERROR_GCE_UNAVAILABLE = "Google Compute integration is not available: "
+
+        // Note: the explicit `timeout` and `tries` below is a workaround. The underlying
+        // issue is that resolving an unknown host on some networks will take
+        // 20-30 seconds; making this timeout short fixes the issue, but
+        // could lead to false negatives in the event that we are on GCE, but
+        // the metadata resolution was particularly slow. The latter case is
+        // "unlikely" since the expected 4-nines time is about 0.5 seconds.
+        // This allows us to limit the total ping maximum timeout to 1.5 seconds
+        // for developer desktop scenarios.
+        private const val MAX_COMPUTE_PING_TRIES = 3
+        private const val COMPUTE_PING_CONNECTION_TIMEOUT_MS = 500
 
         fun deserializeMetadata(json: String) = try {
             GSON.fromJson<Metadata>(json, Metadata::class.java)
@@ -140,5 +164,15 @@ class GoogleMetadataReader(events: EventDispatcher<AgentLifeCycleListener>,
             LOG.debug("Failed to deserialize JSON data ${e.message}", e)
             null
         }
+
+        private val metadataServerUrl: String
+            get() {
+                return System.getenv(GCE_METADATA_HOST_ENV_VAR)?.let {
+                    return@let "http://$it"
+                } ?: DEFAULT_METADATA_SERVER_URL
+            }
+
+        private val instanceMetadataUrl: String
+            get() = metadataServerUrl + METADATA_API_URL
     }
 }
